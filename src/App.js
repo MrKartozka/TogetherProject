ea2e78ae3f6d2c1b32a4369753291e219d8062f4
MrKartozka <mrkartozka@mail.ru>
2023-10-19 19:06:59 +0300

    MrKartozka, Resedate - MainList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import './Сheckbox.css';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState('');
+  const [isChangeUserVisible, setChangeUserVisible] = useState(false);
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const toggleChangeUser = () => {
+    setChangeUserVisible(!isChangeUserVisible);
+  };
+
+  const closeChangeUser = () => {
+    setChangeUserVisible(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='header'>
+      <div className='container'>
+        <div className='navbar'>
+          <div className='hamburger-menu'>
+            <input id='menu__toggle' type='checkbox' />
+            <label className='menu__btn' htmlFor='menu__toggle'>
+              <span></span>
+            </label>
+            <ul className='menu__box'>
+              <li>
+                <a className='menu__item' href='#'>
+                <img src='notes.png' alt='Note Icon' className='icon'></img>
+                Заметки
+                </a>
+              </li>
+              <li>
+                <a className='menu__item' href='#'>
+                <img src='/backstyle.png' alt='Note Icon' className='icon'></img>
+                Стиль
+                </a>
+              </li>
+              <li>
+                <a className='menu__item' href='#'>
+                <img src='/trash.png' alt='Note Icon' className='icon'></img>
+                Корзина
+                </a>
+              </li>
+              <li>
+                <a className='menu__item' href='#'>
+                <img src='/helpful.png' alt='Note Icon' className='icon'></img>
+                Справка
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className='searchline'>
+            <form className='searchform'>
+                <input
+                  type='text'
+                  placeholder='Меня в школе пиздят'
+                  className='search_input'
+                  value={value}
+                  onChange={handleChange}
+                />
+            </form>
+          </div>
+          <div className='addbutton'>
+            <img src='/addbutton.png'></img>
+          </div>
+          <div className='user-profile-container'>
+            <div className='avatar' onClick={toggleChangeUser}>
+              <img src='/avatar.png' className='userprofileimg' alt='User Profile' />
+            </div>
+          {isChangeUserVisible && (
+            <div className='change-user-popup'>
+              <p>Сменить пользователя</p>
+              <button onClick={closeChangeUser}>Закрыть</button>
+            </div>
+          )}
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
